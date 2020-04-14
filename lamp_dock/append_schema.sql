7a525697e6967a7525cf359d7fa6ca8d057e84e9
shibata <pun_pun_3@icloud.com>
2020-04-14 22:29:16 +0900

    sqlの修正

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,18 +1,20 @@
-CREATE TABLE 'history'{
-    'history_id' int(11) NOT NULL,
-    'cart_id' int(11) NOT NULL,
-    'user_id' int(11) NOT NULL,
-    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-};
+CREATE TABLE history(
+    history_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
+    total int(11) NOT NULL,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
-ALTER TABLE 'history'
-    ADD PRIMARY KEY ('history_id');
+ALTER TABLE history
+    ADD PRIMARY KEY (history_id);
 
-ALTER TABLE 'history'
-    MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+ALTER TABLE history
+    MODIFY history_id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 
-CREATE TABLE 'detail'{
-    'history_id' int(11) NOT NULL,
-    'item_id' int(11) NOT NULL,
-    'amount' int(11) NOT NULL,
-};
+CREATE TABLE detail(
+    detail_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    history_id int(11) NOT NULL,
+    item_id int(11) NOT NULL,
+    price int(11) NOT NULL,
+    amount int(11) NOT NULL
+);
